21e6e7e4af7a92326c683464aa6ca28fcbe75eef
Carmohere <lclucianocarmo24@gmail.com>
2025-01-15 23:26:53 -0300

    Finished

--- js/script.js
+++ js/script.js
@@ -27,22 +27,59 @@ function findMovies() {
 
 function displayMovieList(movies) {
     searchlist.innerHTML = "";
-    for (let idx = 0; idx < movies.length; idx++) {
+    movies.forEach(movie => {
         let movieListItem = document.createElement('div');
-        movieListItem.dataset.id = movies[idx].imdbID;
+        movieListItem.dataset.id = movie.imdbID;
         movieListItem.classList.add('search-list-item');
 
-        let moviePoster = movies[idx].Poster !== "N/A" ? movies[idx].Poster : "img/image_not_found.jpg";
+        let moviePoster = movie.Poster !== "N/A" ? movie.Poster : "img/image_not_found.jpg";
 
         movieListItem.innerHTML = `
             <div class="search-item-thumbnail">
                 <img src="${moviePoster}" alt="Poster do filme">
             </div>
             <div class="search-item-info">
-                <h3>${movies[idx].Title}</h3>
-                <p>${movies[idx].Year}</p>
+                <h3>${movie.Title}</h3>
+                <p>${movie.Year}</p>
             </div>
         `;
         searchlist.appendChild(movieListItem);
-    }
+
+        // Adicionar evento de clique para cada item
+        movieListItem.addEventListener('click', async () => {
+            searchlist.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=d3a09049`);
+            const movieDetails = await result.json();
+            displayMovieDetails(movieDetails);
+        });
+    });
 }
+
+function displayMovieDetails(details) {
+    resultGrid.innerHTML = `
+    <div class="movie-poster">
+        <img src="${details.Poster !== "N/A" ? details.Poster : "/img/image_not_found.jpg"}" alt="Poster do filme">
+    </div>
+    <div class="movie-info">
+        <h3 class="movie-title">${details.Title}</h3>
+        <ul class="movie-misc-info">
+            <li class="year">Year: ${details.Year}</li>
+            <li class="rated">Ratings: ${details.Rated}</li>
+            <li class="released">Released: ${details.Released}</li>
+        </ul>
+        <p class="genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class="writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class="actors"><b>Actors:</b> ${details.Actors}</p>
+        <p class="plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class="language"><b>Language:</b> ${details.Language}</p>
+        <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
+    </div>
+    `;
+}
+
+window.addEventListener('click', (event) => {
+    if (event.target.className !== "form-control") {
+        searchlist.classList.add('hide-search-list');
+    }
+});
